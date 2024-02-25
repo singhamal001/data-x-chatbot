327a165942ddb36ee4e12c7e7370583e571ee250
Amal Raj Singh <88592461+singhamal001@users.noreply.github.com>
2024-02-25 20:22:28 +0530

    Add files via upload

---
+++ chain.py
@@ -0,0 +1,21 @@
+import os
+from langchain.document_loaders.pdf import PyPDFLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def chatbot_chain():
+    # os.environ["OPENAI_API_KEY"] = 
+    loader = PyPDFLoader(file_path="bc1_cc3_merged.pdf")
+    documents = loader.load()
+    vectorstore = FAISS.from_documents(documents, OpenAIEmbeddings())
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(),
+                                                  retriever=vectorstore.as_retriever(),
+                                                  memory=memory)
+    return chain
